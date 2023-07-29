ed97a27c498d739dc30617b81bc0656b97f5b234
Antti <antti.astikainen@edu.karelia.fi>
2023-07-29 13:08:17 +0300

    4.16 - 4.19 done

--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -29,7 +29,7 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.get('/', async (request, response) => {
     const user = await User
-        .find({}).populate('blogs', { title: 1, url:1 })
+        .find({}).populate('blogs', { title: 1, url:1, author:1 })
 
     response.json(user)
 })
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -11,11 +11,11 @@ const User = require('../models/user')
 
 
 describe('when there is initially some notes saved', () => {
- beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-})
-   test('blogs are returned as json', async () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    })
+    test('blogs are returned as json', async () => {
         await api
             .get('/api/blogs')
             .expect(200)
@@ -48,10 +48,10 @@ describe('when there is initially some notes saved', () => {
     })
 })
 describe('addition of a new blog', () => {
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-})
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    })
     test('Posting blogs works', async() => {
         const newBlog = {
             title: 'Test blog',
@@ -149,7 +149,7 @@ describe('deleting a blog', () => {
         const blogsAtEnd = await helper.blogsInDb()
 
         //expect(blogsAtEnd).toHaveLength(
-         //   helper.initialBlogs.length)
+            //   helper.initialBlogs.length)
     })
 })
 
@@ -206,14 +206,14 @@ describe('when there is initially one user at db', () => {
         }
 
         await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
         const usersAtEnd = await helper.usersInDb()
         expect(usersAtEnd).toHaveLength(usersAtStart.length +1)
-        
+
         const usernames = usersAtEnd.map(u => u.username)
         expect(usernames).toContain(newUser.username)
     })
@@ -229,18 +229,40 @@ describe('when there is initially one user at db', () => {
             }
 
             const result = await api
-            .post('/api/users')
-            .send(newUser)
-            .expect(400)
-            .expect('Content-Type', /application\/json/)
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
 
             expect(result.body.error).toContain('expected `username` to be unique')
-            
+
             const usersAtEnd = await helper.usersInDb()
             expect(usersAtEnd).toHaveLength(usersAtStart.length)
         })
 
     test('creating fails with proper statuscode and message if username is too short',
+        async () => {
+            const usersAtStart = await helper.usersInDb()
+
+            const newUser = {
+                username: 'ro',
+                name: 'Superuser',
+                password: 'salasana',
+            }
+
+            const result = await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+
+            expect(result.body.error).toContain(`User validation failed: username: Path \`username\` (\`ro\`) is shorter than the minimum allowed length (3).`)
+
+            const usersAtEnd = await helper.usersInDb()
+            expect(usersAtEnd).toHaveLength(usersAtStart.length)
+        })
+
+     test('creating fails with proper statuscode and message if password is too short',
         async () => {
             const usersAtStart = await helper.usersInDb()
 
@@ -251,17 +273,39 @@ describe('when there is initially one user at db', () => {
             }
 
             const result = await api
-            .post('/api/users')
-            .send(newUser)
-            .expect(400)
-            .expect('Content-Type', /application\/json/)
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
 
             expect(result.body.error).toContain('password not given, or it is too short (less than 3 characters)')
-            
+
             const usersAtEnd = await helper.usersInDb()
             expect(usersAtEnd).toHaveLength(usersAtStart.length)
         })
- // TODO : testi käyttäjänimen pituuden testaamiseen ja tyhjään salasanaan   
+
+   test('creating fails with proper statuscode and message if password is not defined ',
+        async () => {
+            const usersAtStart = await helper.usersInDb()
+
+            const newUser = {
+                username: 'root',
+                name: 'Superuser',
+                password: '',
+            }
+
+            const result = await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+
+            expect(result.body.error).toContain('password not given, or it is too short (less than 3 characters)')
+
+            const usersAtEnd = await helper.usersInDb()
+            expect(usersAtEnd).toHaveLength(usersAtStart.length)
+        })
+
 })
 
 
