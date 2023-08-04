1da3995bcbd40cc590507dcbae1dfd54795ce3a4
Antti <antti.astikainen@edu.karelia.fi>
2023-08-04 21:13:40 +0300

    5.3 done

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -5,6 +5,9 @@ import loginService from './services/login'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
@@ -72,6 +75,64 @@ const App = () => {
         window.localStorage.removeItem('loggedBlogappUser')
     }
 
+    const addBlog = (event) => {
+        event.preventDefault()
+        const blogObject = {
+            title: newTitle,
+            author: newAuthor,
+            url: newUrl 
+        }
+
+        blogService
+            .create(blogObject)
+            .then(returnedBlog => {
+                setBlogs(blogs.concat(returnedBlog))
+                setNewTitle('')
+                setNewAuthor('')
+                setNewUrl('')
+            })
+    }
+
+    const handleTitleChange = (event) => {
+        setNewTitle(event.target.value)
+    }
+
+    const handleAuthorChange = (event) => {
+        setNewAuthor(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        setNewUrl(event.target.value)
+    }
+
+    const blogForm = () => (
+        <form onSubmit={addBlog}>
+        <div>
+        title:
+        <input
+            value={newTitle}
+            onChange={handleTitleChange}
+        />
+        </div>
+        <div>
+        author:
+        <input
+            value={newAuthor}
+            onChange={handleAuthorChange}
+        />
+        </div>
+        <div>
+        url:
+        <input
+            value={newUrl}
+            onChange={handleUrlChange}
+        />
+        </div>
+
+           <button type="submit">create</button>
+            </form>
+)
+
 
     if (user === null) {
         return (
@@ -85,8 +146,11 @@ const App = () => {
     return (
         <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in </p>
+        <p>{user.name} logged in        
         <button onClick={() => logOffUser() }>logout</button>
+        </p>
+        <h2>create new</h2>
+        {blogForm()}
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
         )}
