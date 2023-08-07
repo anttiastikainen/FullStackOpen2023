dbbe29e3686578c3ae5ce7ab76b65ab40fa6756c
Antti <antti.astikainen@edu.karelia.fi>
2023-08-07 19:15:21 +0300

    Added consolelogs to blog

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,7 @@
 const Blog = ({blog}) => (
+
   <div>
-    {blog.title} {blog.author}
+    {blog.title} {blog.author} <button onClick={() => console.log(blog.url+"\nlikes "+ blog.likes+"\n"+blog.user.username)}> view </button>
   </div>  
 )
 
