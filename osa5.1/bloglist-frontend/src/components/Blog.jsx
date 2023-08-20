3e9d29662d16a847f2f1d4dbc07b6cab869e8f29
Antti <antti.astikainen@edu.karelia.fi>
2023-08-20 20:04:31 +0300

    Made proxy settings to get data from backend for osa5.1

--- osa5.1/bloglist-frontend/src/components/Blog.jsx
+++ osa5.1/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
+// import [ useState } from 'react'
+//import blogService from '../services/blogs'
+//import PropTypes from 'prop-types'
+
+
+const Blog = ({ blog }) => {
+
+const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    border: '2px solid',
+    marginBottom: 5
+}
+return (
+  <div style={blogStyle}>
     {blog.title} {blog.author}
   </div>  
 )
+
+}
+
 
 export default Blog
--- osa5.1/bloglist-frontend/vite.config.js
+++ osa5.1/bloglist-frontend/vite.config.js
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-        port: 3001
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+      }
     }
-})
+  }
+});
+
