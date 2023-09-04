8cae99430da9001551344e02d22e73cd89bff817
Antti <antti.astikainen@edu.karelia.fi>
2023-09-04 20:49:05 +0300

    fixed some issues on app.js and package.json

--- osa4/app.js
+++ osa4/app.js
@@ -13,6 +13,12 @@ mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
+
 mongoose.connect(config.MONGODB_URI)
     .then (() => {
         logger.info('connected to MongoDB')
